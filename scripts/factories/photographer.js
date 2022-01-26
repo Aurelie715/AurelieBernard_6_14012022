205fba3a1334a7283d3dcd0cd5e5076a76be7305
Aurelie715 <aureliebernard29@gmail.com>
2022-01-26 17:28:36 +0000

    implementation of the index page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,29 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const p = document.createElement("p");
+    p.textContent = `${city}, ${country}`;
+    p.classList.add("photographer-location");
+    const p2 = document.createElement("p");
+    p2.textContent = tagline;
+    p2.classList.add("photographer-tagline");
+    const p3 = document.createElement("p");
+    p3.textContent = `${price}€/jour`;
+    p3.classList.add("photographer-price");
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(p);
+    article.appendChild(p2);
+    article.appendChild(p3);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
